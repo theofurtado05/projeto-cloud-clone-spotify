16c4804f758919065c00b80fd66a77b96364c83d
Theo Furtado <theofurtado05@gmail.com>
2024-06-18 15:53:47 -0300

    deploying

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -21,32 +21,6 @@ export default function RootLayout({
   
   return (
     <>
-    <head>
-    
-          {/* <!-- Meta Pixel Code --> */}
-          <script>{`
-          
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '2090804494632399');
-          fbq('track', 'PageView');
-         
-         
-          <!-- End Meta Pixel Code -->
-            
-          `}
-          </script>
-          <noscript><img height="1" width="1" style={{display:"none"}}
-          src="https://www.facebook.com/tr?id=2090804494632399&ev=PageView&noscript=1"
-          /></noscript>
-          
-    </head>
     <body className="grid grid-cols-1 h-screen overflow-x-hidden">
       <ThemeProvider
         attribute="class"
