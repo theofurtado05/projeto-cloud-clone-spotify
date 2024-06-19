1aab759041fc2ec3342e3a86c7a4ada93317a9f4
Theo Furtado <theofurtado05@gmail.com>
2024-06-19 04:17:41 -0300

    new build please

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -1,5 +1,5 @@
 export function getUrl(path?: string) {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://calm-smoke-03f15421e.5.azurestaticapps.net/'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
     //.... mexer aqui caso de bug na redirecionaçao do site
     const normalizedPath = path && !path.startsWith('/') ? `/${path}` : path || ''
     return `${baseUrl}${normalizedPath}`
