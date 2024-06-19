c8cf18f61bcc7741fe704f7879dd6f7c49c5be05
Theo Furtado <theofurtado05@gmail.com>
2024-06-19 07:26:36 -0300

    n

--- src/lib/get-url.ts
+++ src/lib/get-url.ts
@@ -1,5 +1,5 @@
 export function getUrl(path?: string) {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
+    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://calm-smoke-03f15421e.5.azurestaticapps.net/'
     //.... mexer aqui caso de bug na redirecionaçao do site
     const normalizedPath = path && !path.startsWith('/') ? `/${path}` : path || ''
     return `${baseUrl}${normalizedPath}`
